61a0651385fe75d61ee6d23ef52f9fa2aa29310b
natukin1978 <natukin1978@hotmail.com>
2026-07-02 12:50:34 +0900

    Adjustments, etc.

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 CALLBACK_URL_BOT = "http://localhost:4343/oauth?scopes=" + "%20".join([
-    "moderator:read:followers",
+    "user:read:chat",
+    "user:write:chat",
+    "user:bot",
 ]) + "&force_verify=true"
 
 CALLBACK_URL_OWNER = "http://localhost:4343/oauth?scopes=channel:bot&force_verify=true"
